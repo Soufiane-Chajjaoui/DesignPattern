6025cba6c3bac84391ebc0d8373ac33183f34f81
soufiane chajjaoui <111467805+Soufiane-Chajjaoui@users.noreply.github.com>
2023-12-10 01:08:15 +0100

    Update MyThread2.java

--- src/MyThread2.java
+++ src/MyThread2.java
@@ -3,7 +3,7 @@ public class MyThread2 extends Thread{
     public void run() {
         Singleton singleton = Singleton.GetInstance("Ahmed");
 
-        System.out.println("S1 HashCode is "+ singleton.value);
+        System.out.println("S1 Ahmed is "+ singleton.value);
 
         System.out.println(singleton.hashCode() );
     }
